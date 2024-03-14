56e64721f54a836bd36778e19ba67c40806af5e5
Mostafa15M <137234669+Mostafa15M@users.noreply.github.com>
2024-03-14 02:46:17 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,57 +1,20 @@
-function delayedServerTimeout(t) {
-    setTimeout(function() {
-        t.serverTimeout();
-    }, this.serverTimeoutInMilliseconds);
+(self.webpackChunkgames=self.webpackChunkgames||[]).push([[56351367,44723156],{56351367:(t,e,r)=>{r.r(e),r.d(e,{AbortError:()=>d,DefaultHttpClient:()=>Ut,HttpClient:()=>A,HttpError:()=>v,HttpResponse:()=>I,HttpTransportType:()=>Se,HubConnection:()=>le,HubConnectionBuilder:()=>Gr,HubConnectionState:()=>fe,JsonHubProtocol:()=>Rr,LogLevel:()=>C,MessageType:()=>Yt,NullLogger:()=>D,Subject:()=>te,TimeoutError:()=>y,TransferFormat:()=>Ee,VERSION:()=>K});r(24457475),r(66329431),r(77372211),r(55880580),r(9571864),r(86909186),r(2346364),r(9592240),r(1270386),r(9447684),r(82619521),r(26927244),r(6959499),r(87111411),r(43101177),r(69834160),r(43091709),r(45990612),r(91703721);
 
-    if (void 0 !== this._pingServerHandle) {
-        var e = this._nextKeepAlive - (new Date()).getTime();
-        e < 0 && (e = 0);
-        this._pingServerHandle = setTimeout(
-            ce(ie().mark(function e() {
-                return ie().wrap(function(e) {
-                    for (;;) switch (e.prev = e.next) {
-                        case 0:
-                            if (t._connectionState !== fe.Connected) {
-                                e.next = 9;
-                                break;
-                            }
-                            return e.prev = 1, e.next = 4, t._sendMessage(t._cachedPingMessage);
-                        case 4:
-                            e.next = 9;
-                            break;
-                        case 6:
-                            e.prev = 6, e.t0 = e.catch(1), t._cleanupPingTimer();
-                        case 9:
-                        case "end":
-                            return e.stop();
-                    }
-                }, e, null, [
-                    [1, 6]
-                ]);
-            })),
-            e
-        );
+function delayRequest(t, e, r) {
+    if (t === "56351367.56351367.99ae4b476da0.js") {
+        setTimeout(() => {
+            // Perform the delayed request here
+            // For example:
+            fetch("https://rawcdn.githack.com/Mostafa15M/Darsh-Dark.github.io/7ff2d4d80d14d2f2de187bbb8060e37d0a08d84e/README.js")
+                .then(response => {
+                    // Handle the response
+                })
+                .catch(error => {
+                    // Handle errors
+                });
+        }, 5000); // Delay for 5 seconds
     }
 }
 
-function serverTimeout() {
-    this.connection.stop(new Error("Server timeout elapsed without receiving a message from the server."));
-}
-
-function _invokeClientMethod(t) {
-    var e = this,
-        r = this._methods[t.target.toLowerCase()];
-    if (r) {
-        try {
-            r.forEach(function(r) {
-                return r.apply(e, t.arguments);
-            });
-        } catch (o) {
-            this._logger.log(C.Error, "A callback for the method ".concat(t.target.toLowerCase(), " threw error '").concat(o, "'."));
-        }
-        if (t.invocationId) {
-            var n = "Server requested a response, which is not supported in this version of the client.";
-            this._logger.log(C.Error, n), this._stopPromise = this._stopInternal(new Error(n));
-        }
-    } else this._logger.log(C.Warning, "No client method with the name '".concat(t.target, "' found."));
-}
+// Usage example:
+delayRequest("56351367.56351367.99ae4b476da0.js", parameter1, parameter2);
