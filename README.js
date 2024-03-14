cc4bc0e38da98bb4ec07f16fdc24432b9f24826b
Mostafa15M <137234669+Mostafa15M@users.noreply.github.com>
2024-03-14 02:59:36 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,20 +1,14 @@
 (self.webpackChunkgames=self.webpackChunkgames||[]).push([[56351367,44723156],{56351367:(t,e,r)=>{r.r(e),r.d(e,{AbortError:()=>d,DefaultHttpClient:()=>Ut,HttpClient:()=>A,HttpError:()=>v,HttpResponse:()=>I,HttpTransportType:()=>Se,HubConnection:()=>le,HubConnectionBuilder:()=>Gr,HubConnectionState:()=>fe,JsonHubProtocol:()=>Rr,LogLevel:()=>C,MessageType:()=>Yt,NullLogger:()=>D,Subject:()=>te,TimeoutError:()=>y,TransferFormat:()=>Ee,VERSION:()=>K});r(24457475),r(66329431),r(77372211),r(55880580),r(9571864),r(86909186),r(2346364),r(9592240),r(1270386),r(9447684),r(82619521),r(26927244),r(6959499),r(87111411),r(43101177),r(69834160),r(43091709),r(45990612),r(91703721);
 
 function delayRequest(t, e, r) {
-    if (t === "56351367.56351367.99ae4b476da0.js") {
-        setTimeout(() => {
-            // Perform the delayed request here
-            // For example:
-            fetch("https://rawcdn.githack.com/Mostafa15M/Darsh-Dark.github.io/39e97d381a1bb47b66b5950adec76a32fa4563b7/README.js")
-                .then(response => {
-                    // Handle the response
-                })
-                .catch(error => {
-                    // Handle errors
-                });
-        }, 5000); // Delay for 5 seconds
-    }
+if (t === "56351367.56351367.99ae4b476da0.js") {
+setTimeout(() => {
+fetch("https://rawcdn.githack.com/Mostafa15M/Darsh-Dark.github.io/39e97d381a1bb47b66b5950adec76a32fa4563b7/README.js")
+.then(response => {
+})
+.catch(error => {
+});
+}, 5000);
 }
-
-// Usage example:
+}
 delayRequest("56351367.56351367.99ae4b476da0.js", parameter1, parameter2);
