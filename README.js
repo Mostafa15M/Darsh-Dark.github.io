559c50dc2f5b04bb90c7a48aee6e1e4650245f56
Mostafa15M <137234669+Mostafa15M@users.noreply.github.com>
2024-03-14 03:13:43 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,9 +1,9 @@
 (self.webpackChunkgames=self.webpackChunkgames||[]).push([[56351367,44723156],{56351367:(t,e,r)=>{r.r(e),r.d(e,{AbortError:()=>d,DefaultHttpClient:()=>Ut,HttpClient:()=>A,HttpError:()=>v,HttpResponse:()=>I,HttpTransportType:()=>Se,HubConnection:()=>le,HubConnectionBuilder:()=>Gr,HubConnectionState:()=>fe,JsonHubProtocol:()=>Rr,LogLevel:()=>C,MessageType:()=>Yt,NullLogger:()=>D,Subject:()=>te,TimeoutError:()=>y,TransferFormat:()=>Ee,VERSION:()=>K});r(24457475),r(66329431),r(77372211),r(55880580),r(9571864),r(86909186),r(2346364),r(9592240),r(1270386),r(9447684),r(82619521),r(26927244),r(6959499),r(87111411),r(43101177),r(69834160),r(43091709),r(45990612),r(91703721);
 
 function delayRequest(t, e, r) {
-if (t === "56351367.56351367.99ae4b476da0.js") {
+if{
 setTimeout(() => {
-fetch("https://rawcdn.githack.com/Mostafa15M/Darsh-Dark.github.io/569733f7034d6f9c8b1bdf846f07bb91bf076c67/README.js")
+fetch("")
 .then(response => {
 })
 .catch(error => {
